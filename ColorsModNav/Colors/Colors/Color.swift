edb561df3a1808d1d959e69de6973089019e5805
armajalaruehill <armaja.lacrelle@gmail.com>
2019-10-25 23:56:33 -0500

    hw

---
+++ ColorsModNav/Colors/Colors/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Armaja LaRue-Hill on 10/25/19.
+//  Copyright © 2019 Armaja LaRue-Hill. All rights reserved.
+//
+
+import UIKit
+
+struct Color{
+    
+    let name: String
+    let uiColor: UIColor
+}
